16843033c215976f76de3dd77137695d20dfd1ba
mr-monk-here <131616028+mr-monk-here@users.noreply.github.com>
2023-06-14 23:11:27 +0530

    Update legit.js

--- legit.js
+++ legit.js
@@ -1,6 +1,14 @@
 class legit {
+/*
+LIST OF ALL METHODS
+---valName
+---valNum
+---valPan
+---valEmail
 
+---valInt
 
+*/
     constructor() {
         console.log("legit")
     }
@@ -11,7 +19,7 @@ class legit {
         document.getElementById(giveId)
             .addEventListener("keyup", function () {
                 a = document.getElementById(giveId).value;
-                reg = /\D/;
+                reg = /\d/;
                 if (reg.test(a)) {
                     document.getElementById(giveId).value = '';
                     document.getElementById(msgId).innerText = msg;
@@ -21,8 +29,75 @@ class legit {
             });
     }
 
+    valNum(giveId, msgId, msg){
+        this.checker(/\D/, giveId, msgId, msg);
+    }
+
+    valPan(giveId, msgId, msg){
+        this.panChecker(/[A-Z]{5}[0-9]{4}[A-Za-z]/i, giveId, msgId, msg )
+    }
+
+    valEmail(giveId, msgId, msg){
+        this.emailChecker(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i, giveId, msgId, msg )
+    }
+    
+
+
+    checker(regGive, giveId, msgId, msg){
+        var a;
+        var reg;
+        document.getElementById(giveId)
+            .addEventListener("keyup", function () {
+                a = document.getElementById(giveId).value;
+                reg = regGive;
+                if (reg.test(a)) {
+                    document.getElementById(giveId).value = '';
+                    document.getElementById(msgId).innerText = msg;
+                } else {
+                    document.getElementById(msgId).innerText = '';
+                };
+            });
+    }
+    
+    panChecker(regGive, giveId, msgId, msg){
+        var a;
+        var reg;
+        document.getElementById(giveId)
+            .addEventListener("keyup", function () {
+                a = document.getElementById(giveId).value;
+                reg = regGive;
+                if(a.length >= 10){
+                    if (!reg.test(a)) {
+                        document.getElementById(giveId).value = '';
+                        document.getElementById(msgId).innerText = msg;
+                    } else {
+                        document.getElementById(msgId).innerText = '';
+                    };
+                }else{
+                    document.getElementById(msgId).innerText = msg;
+                }
+            });
+    }
+    emailChecker(regGive, giveId, msgId, msg){
+        var a;
+        var reg;
+        document.getElementById(giveId)
+            .addEventListener("keyup", function () {
+                a = document.getElementById(giveId).value;
+                reg = regGive;
+                    if (!reg.test(a)) {
+                        // document.getElementById(giveId).value = '';
+                        document.getElementById(msgId).innerText = msg;
+                    } else {
+                        document.getElementById(msgId).innerText = '';
+                    };
+                
+            });
+    }
+
+
 }
 
 
 var $l = new legit();
-$l.valName("l-name", "namemsg", "please enter a valid name")
+// $l.valName("l-name", "namemsg", "<please enter a valid name>")
